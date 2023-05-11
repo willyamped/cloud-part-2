c6923fd06149b6f09c1617363f754e1cbaa3d51f
JLR989 <72959468+JLR989@users.noreply.github.com>
2023-05-12 03:14:23 +1000

    Add files via upload

---
+++ Tweet-data-preprocess/Data_analysis.py
@@ -0,0 +1,50 @@
+import ijson
+import pandas as pd
+from collections import Counter
+
+
+T1 = list()
+States = ["New South Wales","Victoria","Queensland","South Australia", 
+          "Western Australia","Tasmania","Northern Territory","Australian Capital Territory"]
+
+requirement_word = ['covid', 'Covid','COVID']
+
+def preprocess(input_file):
+    
+    with open(input_file, "r",encoding='UTF-8') as f:
+        objects = ijson.items(f, "item")
+        obj = next(objects, False)
+        requirement_word = ['covid', 'Covid','COVID']
+        while obj != False:
+            text1 = obj["data"]["text"]
+            
+            city = obj['includes']['places'][0]['full_name']
+            if any(word in text1 for word in requirement_word):
+
+                for i in States:
+                    if i in city:
+                        T1.append(i)
+                    else:
+                        T1.append("Other Territories")
+        
+            obj = next(objects, False)
+    
+        counts = Counter(T1)
+    return counts
+
+def main():
+    # Get file
+    input_file = "twitter-huge.json"
+
+    # Preprocess data
+    result = preprocess(input_file)
+    print(result)
+    result_df = pd.DataFrame.from_records(list(dict(result).items()), columns=['States','COVID-19 mentioned'])
+    print(result_df)
+
+    # Store data
+    result_df.to_csv('C19_states.csv')
+
+# Run the actual program    
+if __name__ == "__main__":
+    main()
