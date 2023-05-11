27838ce8586b7d08a42cca19d1128c672337b7ce
xyliew25 <e0550381@u.nus.edu>
2023-05-12 02:41:10 +1000

    Setup Material UI basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import CoronavirusIcon from '@mui/icons-material/Coronavirus';
+import EmojiFlagsIcon from '@mui/icons-material/EmojiFlags';
+import LanguageIcon from '@mui/icons-material/Language';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+function TabPanel(props) {
+  return (
+    <div hidden={props.value !== props.index}>
+      <Box sx={{ p: 3 }}>
+        {props.children}
+      </Box>
+    </div>
+  );
+}
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {/* NavBar */}
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5">
+            COMP90024
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Box sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: '100vh' }}>
+        {/* SideNav */}
+        <Tabs
+          orientation="vertical"
+          value={value}
+          onChange={handleChange}
+          sx={{ borderRight: 1, borderColor: 'divider' }}
         >
-          Learn React
-        </a>
-      </header>
+          <Tab icon=<CoronavirusIcon /> label="Covid-19" />
+          <Tab icon=<EmojiFlagsIcon /> label="Ukraine" />
+          <Tab icon=<LanguageIcon /> label="Langauge" />
+        </Tabs>
+
+        {/* Content */}
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+      </Box>
     </div>
   );
 }
