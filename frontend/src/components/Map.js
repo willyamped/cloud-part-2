8cdf0549e476b47a1004074ae8fade07f5d00259
xyliew25 <e0550381@u.nus.edu>
2023-05-14 16:20:48 +1000

    Make map zoomable and draggable

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,8 +1,12 @@
 import { useRef, useEffect, useState } from 'react';
-import { select, geoPath, geoMercator, min, max, scaleLinear } from 'd3';
+import { select, geoPath, geoMercator, min, max, scaleLinear, zoom } from 'd3';
+
+const ZOOM_IN_MAX = 8
+const ZOOM_OUT_MAX = 1
 
 function Map({ data, property }) {
   const svgRef = useRef();
+  const gRef = useRef();
   const [selectedCountry, setSelectedCountry] = useState(null);
 
   // will be called initially and on every data change
@@ -19,15 +23,17 @@ function Map({ data, property }) {
 
     // projects geo-coordinates on a 2D plane
     const projection = geoMercator()
-      .fitSize([width, height], selectedCountry || data)
+      .fitSize([width, height], data)
       .precision(100);
 
     // takes geojson data,
     // transforms that into the d attribute of a path element
     const pathGenerator = geoPath().projection(projection);
 
+    const g = select(gRef.current);
+
     // render each country
-    svg
+    g
       .selectAll('.country')
       .data(data.features)
       .join('path')
@@ -40,7 +46,7 @@ function Map({ data, property }) {
       .attr('d', feature => pathGenerator(feature));
 
     // render text
-    svg
+    g
       .selectAll('.label')
       .data([selectedCountry])
       .join('text')
@@ -55,10 +61,19 @@ function Map({ data, property }) {
       .attr('x', 10)
       .attr('y', 25);
 
+    svg.call(zoom()
+      .translateExtent([[0, 0], [width, height]])
+      .scaleExtent([ZOOM_OUT_MAX, ZOOM_IN_MAX])
+      .on('zoom', (event) => {
+        g.attr('transform', event.transform)
+      }));
+
   }, [data, property, selectedCountry]);
 
   return (
-    <svg ref={svgRef} style={{ width: '90vw', height: '90vh' }}></svg>
+    <svg ref={svgRef} style={{ width: '90vw', height: '90vh' }}>
+      <g ref={gRef}></g>
+    </svg>
   );
 }
 
